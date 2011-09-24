9fddd6fe7f9543e1c68afd25f484a69c6186350f
David <david.guo@uwaterloo.ca>
2011-09-24 14:21:57 -0400

    jobmine/internal-css added

---
+++ scripts/jobmine.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import re
+
+cssfile = open('../css/style.css', 'r')
+inputhtml = open('../index.html', 'r')
+outputhtml = open('../jobmine.html', 'w+')
+
+for line in inputhtml:
+    if re.match('(.*)style.css(.*)', line):
+        outputhtml.write('<style type="text/css">\n')
+        for sline in cssfile:
+            outputhtml.write('\t' + sline)
+        outputhtml.write('</style>\n')
+    else:        
+        outputhtml.write(line)
